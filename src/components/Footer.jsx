2f8933cb226f9dacdbad26bdbd42456019ed7a4e
Sofia <sofirocher_@hotmail.com>
2023-11-22 20:29:07 -0300

    faq added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='footer_container'>
       <p className='footer_dev'>Developed by <a target='_blank' href="https://www.instagram.com/srocher_/">srocher.</a></p>
-      <p>&copy; Copyright reserved by Media Growth Hub</p>
+      <p>2023 &copy; Copyright reserved by Media Growth Hub</p>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,67 @@
 import React from 'react'
+import { useState } from 'react'
 import "./Home.css"
 import Metrics from './Metrics.jsx'
 import Footer from './Footer.jsx'
 import Gallery from './Gallery.jsx'
+import FAQ from './FAQ.jsx'
 
 function Home() {
+  const [faqs, setfaqs] = useState([
+    {
+      question: '¿Cuánto tiempo lleva activar esto?',
+      answer: 'En 7 días como MÁXIMO estaremos en condiciones de iniciar con la acción.',
+      open: true
+    },
+    {
+      question: '¿Funciona para mi si mi nicho de mercado es muy específico?',
+      answer: 'Con seguridad, ya que lo primero que hacen nuestros profesionales es estudiar y analizar a fondo tu nicho, para entender de la mejor manera qué es lo que está generando los resultados que deseamos.',
+      open: false
+    },
+    {
+      question: '¿Funciona para mi si no tengo redes sociales o mis redes sociales tienen poca presencia?',
+      answer: 'Justamente nos dedicamos a potenciar las marcas personales y redes sociales de nuestros clientes ya sea que tengan cuentas con pocos seguidores o más destacadas y quieran ir al siguiente nivel.',
+      open: false
+    },
+    {
+      question: '¿Qué riesgos corro entrando al programa?',
+      answer: "El riesgo que corres es NO entrar al programa, ya que seguirás de la misma manera, obteniendo los mismos resultados que tienes ahora. Si quieres los resultados que deseas, te invito a que agendes una llamada gratuita.",      
+      open: false
+    }
+  ]);
+
+  const toggleFAQ = index => {
+    setfaqs(faqs.map((faq, i) => {
+      if (i === index) {
+        faq.open = !faq.open
+      } else {
+        faq.open = false;
+      }
+      return faq;
+    }))
+  }
+
   return (
     <div className='home_container'>
       <div className='home_container-two'>
         <div className='title_container'>
           <h1 className='title_desc'>
-            Cómo agregar 10k, 20k y hasta 30k en 60 días en tu negocio digital, infoproductos o agencia de coaching.
+          Este video de 5 minutos explica cómo ayudamos a empresas y creadores de contenido a destacarse en las redes sociales y a construir audiencia logrando la monetización deseada.
           </h1>
-          <p className='title_p'>
-            Texto descriptivo de la propuesta y cómo le solucionan el problema al cliente. 
-          </p>
         </div>
         <div className="vid_container">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/dvvLhlbpHtI?si=cTebLCeX9CodY383" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"></iframe>
         </div>
      </div>
-      <button className='home_btn'><a target='_blank' href="https://calendly.com/mediagrowthhub/30min">AGENDÁ UNA LLAMADA AHORA!</a></button>
+      <button className='home_btn'><a target='_blank' href="https://calendly.com/mediagrowthhub/30min">AGENDÁ UNA LLAMADA AHORA!</a></button> 
       <Gallery/>
       <Metrics/>
+      <div className="faqs">
+        <h2 className='faqs-title'>Preguntas Frecuentes</h2>
+        {faqs.map((faq, i) => (
+          <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ} />
+        ))}
+      </div>
       <Footer/>
     </div>
   )
