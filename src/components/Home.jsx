10ccc3828896b73cc159c84789fd3ecdd5cfee1c
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2024-01-10 20:25:31 -0300

    Fix FAQ

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "../styles/Home.css"
 import Metrics from './Metrics.jsx'
 import Footer from './Footer.jsx'
@@ -11,86 +11,89 @@ import videoBg from "../assets/background.mp4"
 import Navbar from "./Navbar.jsx"
 import { useTranslation } from 'react-i18next'
 
-
 function Home() {
-  const { t } = useTranslation();
+    const { t } = useTranslation();
 
-  const [faqs, setfaqs] = useState([
-    {
-      question: t("home.faq1"),
-      answer: t("home.ans1"),
-      open: false
-    },
-    {
-      question: t("home.faq2"),
-      answer: t("home.ans2"),
-      open: false
-    },
-    {
-      question: t("home.faq3"),
-      answer: t("home.ans3"),
-      open: false
-    },
-    {
-      question: t("home.faq4"),
-      answer: t("home.ans4"),      
-      open: false
+    const [faqs, setfaqs] = useState([]);
+
+    useEffect(() => {
+        const faqsData = [
+            {
+                question: t("home.faq1"),
+                answer: t("home.ans1"),
+                open: false
+            },
+            {
+                question: t("home.faq2"),
+                answer: t("home.ans2"),
+                open: false
+            },
+            {
+                question: t("home.faq3"),
+                answer: t("home.ans3"),
+                open: false
+            },
+            {
+                question: t("home.faq4"),
+                answer: t("home.ans4"),
+                open: false
+            }
+        ];
+        setfaqs(faqsData);
+    }, [t]);
+
+    const toggleFAQ = index => {
+        setfaqs(faqs.map((faq, i) => {
+            if (i === index) {
+                faq.open = !faq.open
+            } else {
+                faq.open = false;
+            }
+            return faq;
+        }))
     }
-  ]);
 
-  const toggleFAQ = index => {
-    setfaqs(faqs.map((faq, i) => {
-      if (i === index) {
-        faq.open = !faq.open
-      } else {
-        faq.open = false;
-      }
-      return faq;
-    }))
-  }
-
-
-  return (
-    <div className='home_container'>
-        <div className="overlay"></div>
-        <video src={videoBg} autoPlay loop muted playsInline className='background'/>
-        <div className="content">
-          <Navbar />
-          <div className='home_container-two'>
-            <div className='title_container'>
-              <Fade>
-                <h1 className='title_desc'>
-                  {t("home.title")}
-                </h1>
-              </Fade>
+    return (
+        <div className='home_container'>
+            <div className="overlay"></div>
+            <video src={videoBg} autoPlay loop muted playsInline className='background' />
+            <div className="content">
+                <Navbar />
+                <div className='home_container-two'>
+                    <div className='title_container'>
+                        <Fade>
+                            <h1 className='title_desc'>
+                                {t("home.title")}
+                            </h1>
+                        </Fade>
+                    </div>
+                    <div className="vid_container">
+                        <Fade>
+                            <iframe width="860" height="415" src="https://www.youtube.com/embed/dvvLhlbpHtI?si=cTebLCeX9CodY383" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"></iframe>
+                        </Fade>
+                    </div>
+                </div>
+                <button className='home_btn'>
+                    <Zoom>
+                        <a target='_blank' rel='noreferrer' href="https://calendly.com/mediagrowthhub/30min">{t("home.button")}</a>
+                    </Zoom>
+                </button>
             </div>
-            <div className="vid_container">
-              <Fade>
-                <iframe width="860" height="415" src="https://www.youtube.com/embed/dvvLhlbpHtI?si=cTebLCeX9CodY383" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;"></iframe>
-              </Fade>
-            </div>
+            <Gallery />
+            <Servicio />
+            <Metrics />
+            <Conclusion />
+            <Fade>
+                <div className="faqs">
+                    <h2 className='faqs-title'>{t("home.faq-title")}</h2>
+                    {faqs.map((faq, i) => (
+                        <FAQ key={i} faq={faq} index={i} toggleFAQ={toggleFAQ} />
+                    ))}
+                </div>
+            </Fade>
+            <Footer />
         </div>
-              <button className='home_btn'>
-                <Zoom>
-                  <a target='_blank' rel='noreferrer' href="https://calendly.com/mediagrowthhub/30min">{t("home.button")}</a>    
-                </Zoom>
-              </button> 
-        </div>
-      <Gallery/>
-      <Servicio/>
-      <Metrics/>
-      <Conclusion/>
-        <Fade>
-          <div className="faqs">
-            <h2 className='faqs-title'>{t("home.faq-title")}</h2>
-            {faqs.map((faq, i) => (
-              <FAQ key={i} faq={faq} index={i} toggleFAQ={toggleFAQ}/>
-            ))}
-          </div>
-        </Fade>
-      <Footer/>
-    </div>
-  )
+    )
 }
 
 export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,29 @@ import logo from "../assets/logo.png"
 import { Fade } from "react-awesome-reveal";
 import i18n from "../i18n";
 
-function Navbar() { 
+function Navbar() {
 
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng);
-  };
+    const changeLanguage = () => {
+        const currentLanguage = i18n.language;
+        const newLanguage = currentLanguage === 'en' ? 'es' : 'en';
+        i18n.changeLanguage(newLanguage);
+    };
 
-
-  return (
-      <div className='nav_container'>
-        <Fade>
-        <div className='logo_container'>
-          <img className="nav_img" src= {logo} />
-          <p className='nav_p'>MediaGrowth</p>
+    return (
+        <div className='nav_container'>
+            <Fade>
+                <div className='logo_container'>
+                    <img className="nav_img" src={logo} />
+                    <p className='nav_p'>MediaGrowth</p>
+                </div>
+                <div>
+                    <button className='nav_btn' onClick={() => changeLanguage('en')}>
+                        ESP/ENG
+                    </button>
+                </div>
+            </Fade>
         </div>
-        <div>
-          <button className='nav_btn' onClick={() => changeLanguage('en')}>
-              ESP/ENG
-          </button>
-        </div>
-       </Fade>
-      </div>
-    
-  )
+    )
 }
 
 export default Navbar
